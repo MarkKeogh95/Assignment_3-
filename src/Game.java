40dbb2d08959008dabe64238a8d82d2c678cc9e2
MarkKeogh95 <keogher12@gmail.com>
2016-04-19 15:28:41 +0100

    Changes

--- src/Game.java
+++ src/Game.java
@@ -1,25 +1,34 @@
 import java.applet.Applet;
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class Game extends Applet implements Runnable, KeyListener{
 
+	Graphics gfx;
+	Image img;
 	
 	public void init(){
-		
+		this.resize(400, 400);
+		img = createImage(400,400);
+		gfx = img.getGraphics();
 	}
 	
 	public void paint(Graphics g){
+		gfx.setColor(Color.black);
+		gfx.fillRect(0, 0, 400, 400);
 		
+		g.drawImage(img, 0, 0, null);
 	}
 	
 	public void update(Graphics g){
-		
+		paint(g);
 	}
 	
 	public void repaint(Graphics g){
-		
+		paint(g);
 	}
 
 	
