00e1c7e5548de2e207d8b8ce30a02a678caf4749
MarkKeogh95 <keogher12@gmail.com>
2016-04-19 15:19:06 +0100

    Changes

---
+++ src/Game.java
@@ -0,0 +1,45 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Game extends Applet implements Runnable, KeyListener{
+
+	
+	public void init(){
+		
+	}
+	
+	public void paint(Graphics g){
+		
+	}
+	
+	public void update(Graphics g){
+		
+	}
+	
+	public void repaint(Graphics g){
+		
+	}
+
+	
+	public void run() {
+		
+	}
+
+	public void keyTyped(KeyEvent e) {
+		
+	}
+
+	
+	public void keyPressed(KeyEvent e) {
+		
+		
+	}
+
+	
+	public void keyReleased(KeyEvent e) {
+		
+		
+	}
+}
